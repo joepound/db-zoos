10006eeddf6508c190430a9910c6b0a1e476d4ee
joepound <14357797+joepound@users.noreply.github.com>
2019-02-19 17:40:42 -0700

    Completed POST and DELETE request routes

--- mainRoutes.js
+++ mainRoutes.js
@@ -5,7 +5,35 @@ const db = require("./data/dbConfig");
 function route(tableName) {
   const router = express.Router();
 
-  router.get("/", (req, res) => {
+  router.post("/", function(req, res) {
+    console.log(
+      `\nAttempting to POST new database entry to records for ${tableName}...`
+    );
+
+    const { name } = req.body;
+
+    console.log("Checking if name was supplied...");
+    if (name) {
+      db(tableName)
+        .insert({ name })
+        .then(newRow => res.status(201).json(newRow[0]))
+        .catch(err => {
+          if (err.errno === 19) {
+            res
+              .status(400)
+              .json({ error: `Name is not unique in ${tableName} record.` });
+          } else {
+            res.status(500).json(err);
+          }
+        })
+        .finally(console.log(`POST attempt for ${tableName} record finished.`));
+    } else {
+      res.status(400).json({ error: "No name was supplied." });
+      console.log(`POST attempt for ${tableName} record finished.`);
+    }
+  });
+
+  router.get("/", function(req, res) {
     console.log(`\nAttempting to GET all ${tableName}...`);
     db(tableName)
       .then(rows => res.status(200).json(rows))
@@ -18,7 +46,7 @@ function route(tableName) {
       .finally(console.log(`GET all ${tableName} attempt finished.`));
   });
 
-  router.get("/:id", (req, res) => {
+  router.get("/:id", function(req, res) {
     const { id } = req.params;
 
     console.log(`\nAttempting to GET all ${tableName} with ID [${id}]...`);
@@ -27,22 +55,45 @@ function route(tableName) {
       .first()
       .then(row => {
         if (row) {
-          res.status(200).json(rows);
+          res.status(200).json(row);
         } else {
-          res.status(404).json("Supplied ID does not belong to any entry.");
+          res
+            .status(404)
+            .json({ error: "Supplied ID does not belong to any entry." });
         }
       })
       .catch(err => {
         console.log(err);
-        res.status(500).json({
-          err
-        });
+        res.status(500).json(err);
       })
       .finally(
         console.log(
           `GET attempt for any ${tableName} with ID [${id}] finished.`
         )
       );
+  });
+
+  router.delete("/:id", function(req, res) {
+    const { id } = req.params;
+
+    console.log(`\nAttempting to DELETE all ${tableName} with ID [${id}]...`);
+    db(tableName)
+      .where({ id })
+      .del()
+      .then(deletionCount => {
+        if (deletionCount) {
+          res.sendStatus(200);
+        } else {
+          res
+            .status(404)
+            .json({ error: "Supplied ID does not belong to any entry." });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      })
+      .finally(`DELETE attempt for any ${tableName} with ID [${id}] finished.`);
   });
 
   return router;
